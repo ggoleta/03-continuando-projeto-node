49fcc581d9418ec16de3a6d866c7e826348e8019
Rogerio <rogerio_contato@hotmail.com>
2019-09-03 18:44:46 -0300

    chore: provider's list

--- src/routes.js
+++ src/routes.js
@@ -6,13 +6,12 @@ import SessionController from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
 import ProviderController from './app/controllers/ProviderController';
 import AppointmentController from './app/controllers/AppointmentController';
+import ScheduleController from './app/controllers/ScheduleController';
 
 import authMiddleware from './app/middlewares/auth';
 
-
 const routes = new Router();
-const upload = multer(multerCofig)
-
+const upload = multer(multerCofig);
 
 routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
@@ -26,6 +25,8 @@ routes.get('/providers', ProviderController.index);
 
 routes.post('/appointments', AppointmentController.store);
 routes.get('/appointments', AppointmentController.index);
+
+routes.get('/schedule', ScheduleController.index);
 
 routes.post('/files', upload.single('file'), FileController.store);
 
